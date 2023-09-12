7362ed60d4e39528471bd63c4930188ff1f57557
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-09-12 11:07:22 -0300

    Directive Input - Cambiar el color del host

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,17 +6,31 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
 
   private htmlElement?:ElementRef<HTMLElement>;
+  private _color:string = 'red';
+
+  @Input() set color(value:string){
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor( private el:ElementRef<HTMLElement> ) {
     //console.log('contstructor de la Directiva')
     console.log(el);
     this.htmlElement = el;
 
-    this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
+    //this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
   }
 
   ngOnInit(): void {
     console.log("Directiva - ngOnInit");
+
+    this.setStyle();
+  }
+
+  setStyle():void{
+    if (!this.htmlElement) return;
+
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 
 }
