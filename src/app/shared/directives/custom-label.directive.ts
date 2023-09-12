764940fcdbb1ddcd549cf1f651cf1969a3982441
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-09-12 11:27:47 -0300

    Cambiar el mensaje de la etiqueta

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,23 +8,24 @@ export class CustomLabelDirective implements OnInit {
 
   private htmlElement?:ElementRef<HTMLElement>;
   private _color:string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value:string){
     this._color = value;
     this.setStyle();
   }
 
-  constructor( private el:ElementRef<HTMLElement> ) {
-    //console.log('contstructor de la Directiva')
-    console.log(el);
-    this.htmlElement = el;
+  @Input() set errors(value: ValidationErrors | null | undefined){
+    this._errors = value;
+    this.setErrorMessage();
+  };
 
-    //this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
+
+  constructor( private el:ElementRef<HTMLElement> ) {
+    this.htmlElement = el;
   }
 
   ngOnInit(): void {
-    console.log("Directiva - ngOnInit");
-
     this.setStyle();
   }
 
@@ -31,6 +33,35 @@ export class CustomLabelDirective implements OnInit {
     if (!this.htmlElement) return;
 
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage():void{
+    if(!this.htmlElement) return;
+
+    if(!this._errors){
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')){
+      this.htmlElement.nativeElement.innerHTML = 'Este campo es requerido';
+      return;
+    }
+
+    if (errors.includes('minlength')){
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerHTML = `Mínimo ${current}/${min} caracteres`;
+      return;
+    }
+
+    if (errors.includes('email')){
+      this.htmlElement.nativeElement.innerHTML = 'No tiene formato de correo';
+      return;
+    }
   }
 
 }
