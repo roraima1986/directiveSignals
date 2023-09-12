af148f2a5c86888684f87099ec7b2197fa551859
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-09-12 15:07:42 -0300

    Nuestra primera señal - signal

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface MenuItem {
   title: string;
@@ -12,10 +12,17 @@ interface MenuItem {
 })
 export class SideMenuComponent {
 
-  public menuItems: MenuItem[] = [
+  public menuItems = signal<MenuItem[]>([
     {title: 'Contador', route: 'counter'},
     {title: 'Usuario', route: 'user-info'},
     {title: 'Mutaciones', route: 'properties'},
-  ]
+  ]);
+
+  // Manera tradicional
+  /*public menuItems: MenuItem[] = [
+    {title: 'Contador', route: 'counter'},
+    {title: 'Usuario', route: 'user-info'},
+    {title: 'Mutaciones', route: 'properties'},
+  ]*/
 
 }
