31815cf464dc373bf1fd731e8432c875a3a2bd1b
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-09-13 11:13:36 -0300

    manejo de errores y propiedades computadas

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 import { UsersServiceService } from '../../services/users-service.service';
 
@@ -11,6 +11,11 @@ export class UserInfoPageComponent implements OnInit {
 
   private usersServices = inject(UsersServiceService);
   public userId = signal(1);
+  public fullName = computed<string>( () => {
+    if (!this.currentUser()) return 'Usuario no encontrado';
+
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  } );
 
   public currentUser = signal<User|undefined>(undefined);
   public userwasFound = signal(true);
@@ -26,8 +31,16 @@ export class UserInfoPageComponent implements OnInit {
     this.currentUser.set(undefined);
 
     this.usersServices.getUsersById(id)
-      .subscribe(user => {
-        this.currentUser.set(user)
-      })
+      .subscribe({
+        next: (user) => {
+          this.currentUser.set(user);
+          this.userwasFound.set(true);
+        },
+        error: () => {
+          this.userwasFound.set(false);
+          this.currentUser.set(undefined);
+        },
+
+      });
   }
 }
